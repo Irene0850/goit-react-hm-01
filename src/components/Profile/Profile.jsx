5a1bf5d4deab33614b1e878c5a426b9016ad2e61
Iryna Kolomiichenko <159377221+Irene0850@users.noreply.github.com>
2024-09-08 16:59:18 +0300

    new

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,6 @@
 import css from "./Profile.module.css";
 
-const Profile = ({
-  name,
-  tag,
-  location,
-  image,
-  stats,
-  followers,
-  views,
-  likes,
-}) => {
+const Profile = ({ name, tag, location, image, stats }) => {
   return (
     <div className={css.profile}>
       <div className={css.description}>
@@ -22,15 +13,15 @@ const Profile = ({
       <ul className={css.stats}>
         <li className={css.statsItem}>
           <span className={css.label}>Followers</span>
-          <span className={css.quantity}>{followers}</span>
+          <span className={css.quantity}>{stats.followers}</span>
         </li>
         <li className={css.statsItem}>
           <span className={css.label}>Views</span>
-          <span className={css.quantity}>{views}</span>
+          <span className={css.quantity}>{stats.views}</span>
         </li>
         <li className={css.statsItem}>
           <span className={css.label}>Likes</span>
-          <span className={css.quantity}>{likes}</span>
+          <span className={css.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
